a5a3ecfa55c28742170aa614acab0a37f4950979
local <tomaszsewruk@gmail.com>
2021-10-13 18:40:05 +0200

    modyfikacja lab 02

--- LAB_02/src/zad_01.java
+++ LAB_02/src/zad_01.java
@@ -1,4 +1,95 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
+import java.lang.Math.*;
 
 public class zad_01 {
+    public static void main(String[] args){
+//        // zadanie.1
+//
+//        // a)
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("Podaj liczbe naturalna n: ");
+//        int n = scan.nextInt();
+//        int suma = 0;
+//        for(int i=1; i<=n; i++){
+//            System.out.print("Podaj liczbe: ");
+//            int k = scan.nextInt();
+//            suma += k;
+//        }
+//        System.out.print("Suma wczytanych liczb wynosi: " + suma);
+//
+//
+//        // b)
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("Podaj liczbe naturalna n: ");
+//        int n = scan.nextInt();
+//        int iloczyn = 1;
+//        for(int i=1; i<=n; i++){
+//            System.out.print("Podaj liczbe: ");
+//            int k = scan.nextInt();
+//            iloczyn *= k;
+//        }
+//        System.out.print("Iloczyn wczytanych liczb wynosi: " + iloczyn);
+//
+//
+//        // c)
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("Podaj liczbe naturalna n: ");
+//        int n = scan.nextInt();
+//        int suma = 0;
+//        for(int i=1; i<=n; i++){
+//            System.out.print("Podaj liczbe calkowita: ");
+//            int k = scan.nextInt();
+//            suma = suma + Math.abs(k);
+//        }
+//        System.out.print("Suma wartosci bezwzglednych wprowadzonych wartosci wynosi: " + suma);
+//
+//
+//        // d)
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("Podaj liczbe naturalna n: ");
+//        int n = scan.nextInt();
+//        double suma = 0;
+//        for(int i=1; i<=n; i++){
+//            System.out.print("Podaj liczbe calkowita: ");
+//            double k = scan.nextDouble();
+//            k = Math.abs(k);
+//            suma = suma + Math.sqrt(k);
+//        }
+//        System.out.print("Suma pierwiastkow z wartosci bezwzglednych wprowadzonych wartosci wynosi: " + suma);
+//
+//
+//        // e)
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("Podaj liczbe naturalna n: ");
+//        int n = scan.nextInt();
+//        int iloczyn = 1;
+//        for(int i=1; i<=n; i++){
+//            System.out.print("Podaj liczbe calkowita: ");
+//            int k = scan.nextInt();
+//            iloczyn = iloczyn * Math.abs(k);
+//        }
+//        System.out.print("Iloczyn wartosci bezwzglednych wprowadzonych wartosci wynosi: " + iloczyn);
+//
+//
+//        // f)
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("Podaj liczbe naturalna n: ");
+//        int n = scan.nextInt();
+//        double suma = 0;
+//        for(int i=1; i<=n; i++){
+//            System.out.print("Podaj liczbe calkowita: ");
+//            int k = scan.nextInt();
+//            suma = suma + Math.pow((double)k, 2);
+//        }
+//        System.out.print("Suma kwadratow wprowadzonych wartosci wynosi: " + suma);
+
+
+        // g)
+        Scanner scan = new Scanner(System.in);
+
+
+
+
+
+    }
 }
