4305aa84be5488211755dec24af7b5e87351346a
KrystianKierklo <73948612+KrystianKierklo@users.noreply.github.com>
2021-11-24 18:08:35 +0100

    Add files via upload

---
+++ LAB_07/src/Adres_test.java
@@ -0,0 +1,9 @@
+public class Adres_test {
+    public static void main(String[] args){
+        Adres pierwszy = new Adres("Plac Konsulatu Polskiego", 6, "Olsztyn", 10-699);
+        Adres drugi = new Adres("Armii krajowej", 10, "Olsztyn", 11-051);
+
+        pierwszy.pokaz();
+        drugi.pokaz();
+    }
+}
